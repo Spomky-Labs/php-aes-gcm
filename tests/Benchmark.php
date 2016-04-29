f6b1e3b0c24f48e49a8213adc9f8b7e316d75d45
Spomky <Spomky@users.noreply.github.com>
2016-04-29 08:49:07 +0200

    Little benchmark added

---
+++ tests/Benchmark.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2016 Spomky-Labs
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+ 
+ include_once __DIR__.'/../vendor/autoload.php';
+
+use AESGCM\AESGCM;
+
+/**
+ * This function allows developpers to test the encryption performance of the library on their environnement
+ * @param int $nb Number of encryption/decryption to perform
+ */
+function runEncryptionBenchmark($nb = 10000)
+    {
+        $K = hex2bin('feffe9928665731c6d6a8f9467308308feffe9928665731c6d6a8f9467308308');
+        $IV = hex2bin('9313225df88406e555909c5aff5269aa6a7a9538534f7da1e4c303d2a318a728c3c0c95156809539fcf0e2429a6b525416aedbf5a0de6a57a637b39b');
+        $P = hex2bin('d9313225f88406e5a55909c5aff5269a86a7a9531534f7da2e4c303d8a318a721c3c0c95956809532fcf0e2449a6b525b16aedf5aa0de657ba637b39');
+        $A = hex2bin('feedfacedeadbeeffeedfacedeadbeefabaddad2');
+
+        for ($i = 0; $i < $nb; $i++) {
+            AESGCM::encrypt($K, $IV, $P, $A);
+        }
+    }
+}
+
+/**
+ * This function allows developpers to test the decryption performance of the library on their environnement
+ * @param int $nb Number of encryption/decryption to perform
+ */
+function runDecryptionBenchmark($nb = 10000)
+    {
+        $K = hex2bin('feffe9928665731c6d6a8f9467308308feffe9928665731c6d6a8f9467308308');
+        $IV = hex2bin('9313225df88406e555909c5aff5269aa6a7a9538534f7da1e4c303d2a318a728c3c0c95156809539fcf0e2429a6b525416aedbf5a0de6a57a637b39b');
+        $C = hex2bin('5a8def2f0c9e53f1f75d7853659e2a20eeb2b22aafde6419a058ab4f6f746bf40fc0c3b780f244452da3ebf1c5d82cdea2418997200ef82e44ae7e3f');
+        $A = hex2bin('feedfacedeadbeeffeedfacedeadbeefabaddad2');
+        $T = hex2bin('a44a8266ee1c8eb0c8b5d4cf5ae9f19a');
+
+        for ($i = 0; $i < $nb; $i++) {
+            AESGCM::decrypt($K, $IV, $C, $A, $T);
+        }
+    }
+}
+
+print_r('################################'.PHP_EOL);
+print_r('# AES-GCM ENCRYPTION BENCHMARK #'.PHP_EOL);
+print_r('################################'.PHP_EOL);
+
+$time_start = microtime(true);
+runEncryptionBenchmark();
+$time_end = microtime(true);
+
+$time = ($time_end - $time_start) / $nb * 1000;
+printf('%f milliseconds/encryption '.PHP_EOL, $time);
+
+print_r('################################'.PHP_EOL);
+print_r('# AES-GCM DECRYPTION BENCHMARK #'.PHP_EOL);
+print_r('################################'.PHP_EOL);
+
+$time_start = microtime(true);
+runDecryptionBenchmark();
+$time_end = microtime(true);
+
+$time = ($time_end - $time_start) / $nb * 1000;
+printf('%f milliseconds/decryption '.PHP_EOL, $time);
+
+print_r('################################'.PHP_EOL);
